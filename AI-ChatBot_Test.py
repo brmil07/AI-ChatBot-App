1031af6b97894cc5cc1bfbce2c01f1ce9180cda0
brmil07 <username@users.noreply.github.com>
2024-08-24 13:18:53 +0200

    added interactive user input for the chatbot

--- AI-ChatBot_Test.py
+++ AI-ChatBot_Test.py
@@ -1,7 +1,37 @@
-from langchain_community.llms import Ollama
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
 
-llm = Ollama(model="llama3.1")
 
-response = llm.invoke("Tell me a joke!")
+def init_model():
+    template = """
+    Answer the question below.
+    Here is the conversation history: {context}
+    Question: {question}
+    Answer:
+    """
 
-print(response)
+    model = OllamaLLM(model="llama3.1")
+    prompt = ChatPromptTemplate.from_template(template)
+    chain = prompt | model
+    return chain
+
+
+def handle_conversation(chain):
+    context = ""
+    print("Welcome to the AI ChatBot App!\nType 'exit' to quit.")
+    while True:
+        user_input = input("You: ")
+        if user_input.lower() == "exit":
+            break
+        result = chain.invoke({"context": context, "question": user_input})
+        print("Bot: ", result)
+        context += f"\nUser: {user_input}\nAI: {result}"
+
+
+def main():
+    chain = init_model()
+    handle_conversation(chain)
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# AI-ChatBot-App
+# **AI-ChatBot-App**
 AI powered ChatBot App that can be used on local machine
+
+# **Getting Started**
+Clone the Repository:
+```bash
+git clone https://github.com/brmil07/AI-ChatBot-App.git
+```
+Navigate to the folder:
+```bash
+cd AI-ChatBot-App
+```
+Install Ollama in your local machine, please visit: [Ollama](https://ollama.com/) for more information.
+
+### **Tested on**:
+- Windows 11
+- VS Code
+- Python 3.12
+
+# **References**
+- [Ollama](https://ollama.com/)
+- [Create a Local Python AI Chatbot in Minutes using Ollama](https://www.youtube.com/watch?v=d0o89z134CQ&list=WL&index=10&t=31s)
